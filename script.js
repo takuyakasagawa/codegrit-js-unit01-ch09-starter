ec2c06b7624cf5628ece4da28a695b5585385be7
Atsuhiro Teshima <hiro@lingualbox.com>
2018-12-18 23:53:34 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+//画像情報のオブジェクトを格納した配列
+const picArray = [
+{src: '', title: ''},
+{src: '', title: ''},
+{src: '', title: ''},
+{src: '', title: ''},
+{src: '', title: ''},
+{src: '', title: ''}
+];
+
+
+//スライドショーが再生中かどうかを記録しておくためのグローバル変数
+let playingID = 0;
+
+//写真を切り替える関数
+//関数名「changePicture」
+
+
+// ボタンを押してスライドショーを再生・停止する関数
+//関数名「playSlidedeshow」
