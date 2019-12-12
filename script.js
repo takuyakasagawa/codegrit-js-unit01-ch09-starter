e63a9e40a19a4182bb9a2dfd8b6feb7425ede448
takuyakasagawa <kasa.kasa.0062362@gmail.com>
2019-12-13 07:38:22 +0900

    edited JS

--- script.js
+++ script.js
@@ -13,10 +13,11 @@ const picArray = [
 
 //スライドショーが再生中かどうかを記録しておくためのグローバル変数
 let playingID = 0;
-
+picArray[0].title
+document.getElementById('pics').src
+document.getElementById('pic-title').innerHTML
 //写真を切り替える関数
 //関数名「changePicture」
-
 
 // ボタンを押してスライドショーを再生・停止する関数
 //関数名「playSlidedeshow」
